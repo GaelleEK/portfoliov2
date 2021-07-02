8ab164cd8fb3113ddc2a74a5e3a11a2615968d72
Gaelle EK <elkadaouigaelle@gmail.com>
2021-07-02 11:11:19 +0200

    🎉 Initial Commit: Begin new portfolio with Vue

---
+++ src/components/Competences.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="block">
+    <div class="columns">
+      <div class="column hover-custom">
+        <div class="card">
+          <figure class="card-image is-96x96">
+            <i class="fab fa-html5"></i>
+          </figure>
+        </div>
+      </div>
+      <div class="column hover-custom">
+        <div class="card">
+          <figure class="card-image is-96x96">
+            <i class="fab fa-css3-alt"></i>
+          </figure>
+        </div>
+      </div>
+      <div class="column hover-custom">
+        <div class="card">
+          <figure class="card-image is-96x96">
+            <i class="fab fa-php"></i>
+          </figure>
+        </div>
+      </div>
+      <div class="column hover-custom">
+        <div class="card">
+          <figure class="card-image is-96x96">
+            <i class="fab fa-js"></i>
+          </figure>
+        </div>
+      </div>
+      <div class="column hover-custom">
+        <div class="card">
+          <figure class="card-image is-96x96">
+            <i class="fab fa-github"></i>
+          </figure>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+
+
+</script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <router-link class="navbar-item" to="/">
+        <img id="logo" class="image" src="../assets/img/LogoGEK1.png" />
+      </router-link>
+
+      <a id="burger" role="button" class="navbar-burger" :class="isActive" @click="open">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbar" class="navbar-menu" :class="isActive">
+      <div class="navbar-start">
+        <router-link to="/" class="navbar-item">Home</router-link>
+        <router-link to="/about" class="navbar-item">About</router-link>
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link"> More </a>
+
+          <div class="navbar-dropdown">
+            <a class="navbar-item"> About </a>
+            <a class="navbar-item"> Jobs </a>
+            <a class="navbar-item"> Contact </a>
+            <hr class="navbar-divider" />
+            <a class="navbar-item"> Report an issue </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light"> Log in </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+const Navigation = defineComponent({
+  data() {
+    return {
+      isActive: "",
+    };
+  },
+  methods: {
+    open() {
+      if (this.isActive === "") {
+        this.isActive = "is-active";
+      } else {
+        this.isActive = "";
+      }
+    },
+  },
+});
+export default Navigation;
+</script>
